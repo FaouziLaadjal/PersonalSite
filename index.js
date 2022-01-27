524105bffaa4f6f46583b28dc6917b5b0043a304
FaouziLaadjal <95627938+FaouziLaadjal@users.noreply.github.com>
2022-01-27 18:27:28 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,5 +1,18 @@
-document.getElementById("navbtn").addEventListener("click", function () {
-  var element = document
-    .getElementById("navbarlinks")
-    .classList.toggle("random");
-});
+var btn = document.getElementById("navbtn");
+var navbar = document.getElementById("navbarlinks");
+function random() {
+  navbar.classList.toggle("random");
+}
+function outside() {
+  navbar.classList.remove("random");
+}
+btn.onclick = random;
+for (var i = 0; i < 4; i++) {
+  var fouzi = document.querySelectorAll(".a")[i];
+  fouzi.addEventListener("click", random);
+}
+document.onclick = function (e) {
+  if (e.target.id !== "navbtn" && e.target.id !== "navbtn") {
+    outside();
+  }
+};
